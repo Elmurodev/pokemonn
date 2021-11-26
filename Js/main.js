731d012a4530a938762522dccdca3cf73172594f
elmurodev <Elmurod0298@gmail.com>
2021-11-26 22:09:48 +0500

    Pokemon

---
+++ Js/main.js
@@ -0,0 +1,103 @@
+var userName = "Elmurod";
+
+
+
+
+
+console.log("Salom");
+// console.log(userName);
+
+// String
+var studentAge = "23";
+//float 23,5 
+
+// Number
+var studentPhoneNumber = "+998(94)400-99-30";
+console.log(studentPhoneNumber ,typeof studentPhoneNumber);
+
+//Boolean
+var isStudent = false;
+console.log(isStudent , typeof isStudent);
+
+
+
+//Object
+var studentAwards = {
+    faxriyYorliq: true,
+    MaqtovYorliq: false
+};
+
+console.log(studentAwards , typeof studentAwards);
+//Bu ham Object type ismi Array
+var studentMarks =[5, 4, 3, 2, 3, 2,]
+
+console.log(studentMarks , typeof studentMarks);
+
+
+
+//Undefined
+var studentKind1 = null;          
+console.log(studentKind1 ,typeof studentKind1); 
+
+var demoName = 'Nurbek';
+var demoName1 = "G`iyos";
+var demoName2 = 'Napoleo n"2';
+
+console.log(10 + 10 );
+
+//js concatenation
+console.log("Hello" + " " + "All" );
+
+//js concatenation
+console.log( 55 + 4  + 44 );
+var salom = "SAMO" / "4";
+console.log(typeof typeof typeof salom);
+
+console.log("Ismingizni kiriting");
+
+var name = prompt ("Ismingizni kiriting");
+var surname = prompt ("Familyangizni kiriting");
+var age    = prompt ("Yoshingizni kiriting");
+var study  = prompt ("Qayerda o`qishiysiz");
+var work  = prompt  ("Qayerda ishlaysiz");
+var place  = prompt  ("Qayerdansiz");
+var from  = prompt ("Xozir qayerda turasiz")
+var character = prompt ("Xarakteringiz qanaqa");
+var hobby   = prompt  ("Hobyingiz qanaqa");
+
+alert(
+    "Ismingiz:" + " " + name + "\n" +
+    "Familyangiz:" + " " + surname + "\n" +
+    "Yoshingiz:" + " " + age + "\n" +
+    "Qayerda o`qiysiz:" + " " + study + "\n" + 
+    "Qayerda ishlaysiz:" + " " + work + "\n" +
+    "Qayerdansiz:" + " " + place + "\n" +
+    "Xozirda qayerda turasiz:" + " " + from + "\n" +
+    "Xarakteringiz qanaqa:" + " " + character + "\n" +
+    "Hobyingiz qanaqa:" + " " + hobby + "\n" 
+);
+
+
+
+// var yoshingiz = prompt ("Yoshingiz");
+// alert (yoshingiz);
+// console.log (yoshingiz);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
